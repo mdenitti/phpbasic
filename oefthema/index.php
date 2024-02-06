8d2d6bb2842a071cc176f69ae4f949771eb1babd
mdenitti <massimodn77@gmail.com>
2024-02-06 11:32:45 +0100

    $_GET Superglobal case prepopulate input fields

--- oefthema/index.php
+++ oefthema/index.php
@@ -14,14 +14,18 @@
        <div class="row">
             <div class="col-md">
 
+            <?php 
+                print_r($_GET);
+            ?>
+
             <h1>Hello, Themadag</h1>
                 <p>
                 <h3>Bereid je voor op een onvergetelijk avontuur boordevol ontdekkingen en verrassingen.</h3> Of je nu gepassioneerd bent door exotische bestemmingen, culturele hoogtepunten of avontuurlijke reizen, onze themadag belooft een bron van inspiratie te zijn voor jouw volgende reis. Schrijf je nu in en maak je klaar om meegevoerd te worden op een reis door de fascinerende wereld van reizen en avontuur!
                 </p>
                 <form action="submit.php" method="post">
-                    <input class="form-control mt-2" type="text" name="name" placeholder="Geef uw naam">
-                    <input class="form-control mt-2" type="email" name="email" placeholder="Geef uw email">
-                    <input class="form-control mt-2" type="text" name="tel" placeholder="Geef uw telefoon">
+                    <input class="form-control mt-2" type="text" name="name" value = "<?php if(isset($_GET['name'])) { echo $_GET['name']; }?>" placeholder="Geef uw naam">
+                    <input class="form-control mt-2" type="email" name="email" value = "<?php if (isset($_GET['email'])) { echo $_GET['email'];}?>" placeholder="Geef uw email">
+                    <input class="form-control mt-2" type="text" name="tel"  value = "<?php if (isset($_GET['tel'])) { echo $_GET['tel']; }?>" placeholder="Geef uw telefoon">
                     <hr>
                     <?php 
                     $locations = ["Genk - Thailand", "Hasselt - Griekenland", "Zonhoven - Mexico", "Oudsbergen - Italië", "Vlaardingen - Nederland"];
