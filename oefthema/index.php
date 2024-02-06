d7b083832e9e4ff247a2a62e1fa887d340778833
mdenitti <massimodn77@gmail.com>
2024-02-06 10:19:17 +0100

    created exercise v1.0.0

---
+++ oefthema/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Themadag</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <div class="container">
+       <div class="row">
+            <div class="col-md">
+
+            <h1>Hello, Themadag</h1>
+                <p>
+                <h3>Bereid je voor op een onvergetelijk avontuur boordevol ontdekkingen en verrassingen.</h3> Of je nu gepassioneerd bent door exotische bestemmingen, culturele hoogtepunten of avontuurlijke reizen, onze themadag belooft een bron van inspiratie te zijn voor jouw volgende reis. Schrijf je nu in en maak je klaar om meegevoerd te worden op een reis door de fascinerende wereld van reizen en avontuur!
+                </p>
+                <form action="submit.php" method="post">
+                    <input class="form-control mt-2" type="text" name="name" placeholder="Geef uw naam">
+                    <input class="form-control mt-2" type="email" name="email" placeholder="Geef uw email">
+                    <input class="form-control mt-2" type="text" name="tel" placeholder="Geef uw telefoon">
+                    <hr>
+                    <?php 
+                    $locations = ["Genk - Thailand", "Hasselt - Griekenland", "Zonhoven - Mexico", "Oudsbergen - Italië", "Vlaardingen - Nederland"];
+                    print_r($locations);
+                    ?>
+                        <select class="form-select mt-2" name="locaties">
+                            <?php 
+                                foreach ($locations as $location) {
+                                    echo "<option value='$location'>$location</option>";
+                                }
+                            ?>
+                        </select>
+                    
+                    <input class="btn btn-primary mt-2" type="submit" name="submit">
+                    <input class="btn btn-danger mt-2" type="reset" name="reset">
+                </form>
+            </div>
+       </div>
+    </div>
+
+</body>
+</html>
---
+++ oefthema/submit.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Themadag</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php 
+    // print_r($_POST);
+    // assign values to variables from POST
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $locaties = $_POST['locaties'];
+
+    function saveStringToFile($name, $email, $tel, $locaties) {
+        // open or create a new file and append to it
+        $file = fopen("bookings.csv", "a");
+        // provice a prefered format for the date object
+        $currendate = date('Y-m-d H:i:s');
+        // write to file and close... use "\r\n" for new line
+        $string = $currendate . "," . $name . "," . $email . "," . $tel . "," . $locaties . "\r";
+        fwrite($file, $string);
+        fclose($file);
+    }
+    
+    saveStringToFile($name, $email, $tel, $locaties);
+
+   ?>
+  <div class="container">
+       <div class="row">
+            <div class="col-md">
+
+            <h1>Bedankt voor uw inschrijving, <?php echo $name; ?></h1>
+            <h3>Wij verwachten u in <?php echo $locaties; ?></h3>
+            <p>Hier is de gegevens die u heeft ingevuld:</p>
+            <ul>
+                <li>Naam: <?php echo $name; ?></li>
+                <li>Email: <?php echo $email; ?></li>
+                <li>Telefoon: <?php echo $tel; ?></li>
+                <li>Locatie: <?php echo $locaties; ?></li>
+            </ul>
+
+            <a class='btn btn-primary' href="index.php">Nieuwe inschrijving</a>
+        </div>
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
