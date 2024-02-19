604613ca8db0cb36f7c1de5d04bf0df0c86758a5
mdenitti <massimodn77@gmail.com>
2024-02-19 13:02:08 +0100

    added method to assign the $tday_id to a tday_name by executing a quick OOP query lookup

--- oefthema/submit.php
+++ oefthema/submit.php
@@ -34,22 +34,34 @@
         fclose($file);
     }
     
+    function get_tday_name($tday_id) {
+        global $conn;
+        $sql = "SELECT name FROM tdays WHERE id = $tday_id";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $tday_name = $row['name'];
+        return $tday_name;
+    }
+
     saveStringToFile($name, $email, $tel, $tday_id);
     store ($name, $email, $tel, $tday_id);
-    
+
+    // get themadag name from database based on tday_id;
+    $tday_name = get_tday_name($tday_id);
+
    ?>
   <div class="container">
        <div class="row">
             <div class="col-md">
 
             <h1>Bedankt voor uw inschrijving, <?php echo $name; ?></h1>
-            <h3>Wij verwachten u in <?php echo $tday_id; ?></h3>
+            <h3>Wij verwachten u in <?php echo $tday_name; ?></h3>
             <p>Hier is de gegevens die u heeft ingevuld:</p>
             <ul>
                 <li>Naam: <?php echo $name; ?></li>
                 <li>Email: <?php echo $email; ?></li>
                 <li>Telefoon: <?php echo $tel; ?></li>
-                <li>Locatie: <?php echo $tday_id; ?></li>
+                <li>Locatie: <?php echo $tday_name; ?></li>
             </ul>
 
             <a class='btn btn-primary' href="index.php">Nieuwe inschrijving</a>
