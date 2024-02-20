bc746cf0fdb1d219b0d04abd16882906f0d51dd3
Adam Gazdiev <a.gazdiev@gmail.com>
2024-02-20 15:40:34 +0100

    User authorization check

--- oefthema/index.php
+++ oefthema/index.php
@@ -1,4 +1,15 @@
-<?php include 'global.php'; ?>
+<?php 
+include 'global.php';
+session_start(); // Start session
+
+// Check if the user is logged in
+if(!isset($_SESSION['user_id'])) {
+    // User is not logged in, redirect to the login page
+    header('Location: login.php');
+    exit;
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,10 +21,10 @@
   <body>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
     <div class="container">
        <div class="row">
             <div class="col-md">
+            <a href="logout.php">Log out</a>
 
             <?php 
                 // example URL: http://localhost:8000/index.php?name=Tante%20Frieda&email=frieda@mail.com&tel=028309283098
