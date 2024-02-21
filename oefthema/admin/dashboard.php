39d3c6999f9059c70f135c43c24bf0dc1524a57a
mdenitti <massimodn77@gmail.com>
2024-02-21 11:25:51 +0100

    Added delete button and chartjs integration

--- oefthema/admin/dashboard.php
+++ oefthema/admin/dashboard.php
@@ -5,7 +5,21 @@
 <h1><span class="fat">Dashboard</span>Admin</h1>
 <h2>Welcome <?php echo $_SESSION['name']?></h2>
 <a href="logout.php" class="btn btn-danger mb-3">Logout</a>
+
+
+<div class="row">
+    <div class="col-md-3"><h3>MyChart</h3></div>
+    <div class="col-md-9">
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
+</div>
+
+
 <p>I am allowed to be here<p>
+
+
 
 <?php 
 $sql = "SELECT
@@ -21,9 +35,7 @@ JOIN
 tdays t ON t.id = r.tday_id;
 ";
 $results = mysqli_query($conn, $sql);
-
 ?>
-
 <table class="table">
   <thead>
     <tr>
@@ -32,6 +44,7 @@ $results = mysqli_query($conn, $sql);
       <th scope="col">Email</th>
       <th scope="col">Tel</th>
       <th scope="col">Tday</th>
+      <th scope="col">Options</th>
     </tr>
   </thead>
   <tbody>
@@ -42,12 +55,33 @@ $results = mysqli_query($conn, $sql);
             <td><?php echo $result['email'] ?></td>
             <td><?php echo $result['tel'] ?></td>
             <td><?php echo $result['themeday'] ?></td>
+            <td><a href="delete.php?id=<?php echo $result['id'] ?>'" class="btn btn-danger">Delete</a></td>
         </tr>
    <?php } ?>
-    
-
   </tbody>
 </table>
 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
 
 <?php include 'footer.php'; ?>
--- oefthema/admin/header.php
+++ oefthema/admin/header.php
@@ -13,6 +13,7 @@
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
     ></script>
     <link rel="stylesheet" href="../style.css" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
   <body>
   <div class="container-fluid">
