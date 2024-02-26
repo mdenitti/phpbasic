6536450a5d57667fefa4510d8ab1e8f16ba64022
mdenitti <massimodn77@gmail.com>
2024-02-26 10:17:26 +0100

    added setter with conditional check and first use of self referencing $this

--- oop/basics.php
+++ oop/basics.php
@@ -1,18 +1,28 @@
 <?php 
 
 class Auto {
-    public $wheels = 4;
+    public $wheels;
+    public function setWheels($wheels) {
+        // set the value of the wheels
+        if ($wheels > 0) {
+            $this->wheels = $wheels;
+        } else {
+            $this->wheels = "- Bruh u need at least one wheel -";
+        }
+    }
 
     public function drive() {
-        return "i am drving";
+        return "i am drving a car with $this->wheels wheels";
     }
 }
 
 // plain object intantiation
 $auto = new Auto;
 // accessible from outside with the public keyword in our class
+echo $auto->setWheels(0);
 echo $auto->drive();
-echo $auto->wheels;
+
+
 
 
 
